8ac54946399ce5897cfd5ae41159bf7eba4ff94b
Timothee Pierre-Andre Frily <tfrily@c3r2s4.42lausanne.ch>
2024-06-13 16:00:42 +0200

    Add test

--- src/main/main_tim.c
+++ src/main/main_tim.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include "../../inc/cub3D.h"
+
 int main()
 {
-	printf("HELLO WORLD TIM");
+	void *mlx;
+	void *mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx,1920 ,1080 , "SALUT ALEXIS");
+	mlx_loop(mlx);
 	return (0);
 }
