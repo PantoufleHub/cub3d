3728ebf9c16ce5f447b06a47f070b4b2ba18f3f7
Timothee Pierre-Andre Frily <tfrily@c3r3s5.42lausanne.ch>
2024-07-29 15:56:44 +0200

    Clean the file movment.c
    - Create pos_update_w and pos_update_s, these functions will update
    the coordonate of the player
    - Put the code to the norm, exept for the 42 header

--- src/render/movment.c
+++ src/render/movment.c
@@ -1,61 +1,60 @@
-// mlx_hook(void *win_ptr, int x_event, int x_mask, int (*funct)(), void *param)
 #include "../../inc/cub3D.h"
-#include <math.h>
-#include <stdio.h>
 
-int is_move(int x, int y , t_data *data)
+int	is_move(int y, int x, t_data *data)
 {
-  // NOTE: RETURN TRUE if a the next position is a '0'. False if not
-  if (data->map[x][y] == '0')
-    return (TRUE);
-  return (FALSE);
+	if (data->map[y][x] == '0')
+		return (TRUE);
+	return (FALSE);
 }
 
-int ft_keypress(int key, t_data *data)
+void	pos_update_w(t_data **data)
 {
-  if (key == K_W)
-  {
-    data->player.playerCos = cos(degreetoRadiant(data->player.angle)) * SPEED;
-    data->player.playerSin = sin(degreetoRadiant(data->player.angle)) * SPEED;
+	(*data)->player.playerCos = cos_degree((*data)->player.angle) * SPEED;
+	(*data)->player.playerSin = sin_degree((*data)->player.angle) * SPEED;
+	(*data)->player.newX = (*data)->player.x + (*data)->player.playerCos;
+	(*data)->player.newY = (*data)->player.y + (*data)->player.playerSin;
+	if (is_move((*data)->player.newY, (*data)->player.newX, (*data)))
+	{
+		(*data)->player.x = (*data)->player.newX;
+		(*data)->player.y = (*data)->player.newY;
+	}
+}
 
-    data->player.newX = data->player.x + data->player.playerCos;
-    data->player.newY = data->player.y + data->player.playerSin;
-    if (data->map[(int)data->player.newY][(int)data->player.newX] == '0')
-    {
-      data->player.x = data->player.newX;
-      data->player.y = data->player.newY;
-    }
-  }
-  if (key == K_S)
-  {
-    data->player.playerCos = cos(degreetoRadiant(data->player.angle)) * SPEED;
-    data->player.playerSin = sin(degreetoRadiant(data->player.angle)) * SPEED;
+void	pos_update_s(t_data **data)
+{
+	(*data)->player.playerCos = cos_degree((*data)->player.angle) * SPEED;
+	(*data)->player.playerSin = sin_degree((*data)->player.angle) * SPEED;
+	(*data)->player.newX = (*data)->player.x - (*data)->player.playerCos;
+	(*data)->player.newY = (*data)->player.y - (*data)->player.playerSin;
+	if (is_move((*data)->player.newY, (*data)->player.newX, (*data)))
+	{
+		(*data)->player.x = (*data)->player.newX;
+		(*data)->player.y = (*data)->player.newY;
+	}
+}
 
-    data->player.newX = data->player.x - data->player.playerCos;
-    data->player.newY = data->player.y - data->player.playerSin;
-    if (data->map[(int)data->player.newY][(int)data->player.newX] == '0')
-    {
-      data->player.x = data->player.newX;
-      data->player.y = data->player.newY;
-    }
-  }
-  if (key == K_A)
-  {
-    data->player.angle -= ROTSPEED;
-    printf("Player Angle: %f",data->player.angle);
-  }
-  if (key == K_D)
-  {
-    data->player.angle += ROTSPEED;
-    printf("Player Angle: %f",data->player.angle);
-  }
-  refresh(data);
-  return (0);
+int	ft_keypress(int key, t_data *data)
+{
+	if (key == K_W)
+		pos_update_w(&data);
+	if (key == K_S)
+		pos_update_s(&data);
+	if (key == K_A)
+	{
+		data->player.angle -= ROTSPEED;
+		printf("Player Angle: %f\n", data->player.angle);
+	}
+	if (key == K_D)
+	{
+		data->player.angle += ROTSPEED;
+		printf("Player Angle: %f\n", data->player.angle);
+	}
+	refresh(data);
+	return (0);
 }
 
 int	ft_controls(t_data *data)
 {
-  mlx_hook(data->mlx.mlx_win, E_KEYPRESS, 1L<<0, ft_keypress, data);
-  // mlx_hook(mlx->mlx_win, 17, 0, ft_buttonquit, data);
-  return (0);
+	mlx_hook(data->mlx.mlx_win, E_KEYPRESS, 1L << 0, ft_keypress, data);
+	return (0);
 }
