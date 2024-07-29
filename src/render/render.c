b7ddffddb1e7c15e1e187f383ead1c504d421969
Timothee Pierre-Andre Frily <tfrily@c3r3s5.42lausanne.ch>
2024-07-29 10:15:02 +0200

    Norm ok on render.c

--- src/render/render.c
+++ src/render/render.c
@@ -1,6 +1,6 @@
 #include "../../inc/cub3D.h"
 
-void render(t_data *data, double distance)
+void	render(t_data *data, double distance)
 {
 	t_line	wall;
 	t_line	ceiling;
@@ -11,9 +11,8 @@ void render(t_data *data, double distance)
 	ceiling.drawStart = 0;
 	ceiling.drawEnd = wall.drawStart;
 	floor.drawStart = wall.drawEnd;
-	floor.drawEnd   = HEIGHT;
+	floor.drawEnd = HEIGHT;
 	color = create_trgb(0, 255, 0, 0);
-
 	pixel_put_line(data->img, data->x, ceiling, create_trgb(0, 255, 255, 255));
 	pixel_put_line(data->img, data->x, wall, color);
 	pixel_put_line(data->img, data->x, floor, create_trgb(0, 0, 255, 0));
@@ -26,14 +25,13 @@ void	refresh(t_data *data)
 	data->ray.rayAngle = data->player.angle - data->player.halfFov;
 	data->x = 0;
 	clear_screen(data);
-	while(data->x < WIDTH)
+	while (data->x < WIDTH)
 	{
-		// wallHeight = (data->screen.halfHeight / distance);
 		distance = raycasting(data);
 		render(data, distance);
-		//increment
 		data->ray.rayAngle += data->raycast.incrAngle;
 		data->x += 1;
 	}
-	mlx_put_image_to_window(data->mlx.mlx, data->mlx.mlx_win, data->img.img, 0, 0);
+	mlx_put_image_to_window(data->mlx.mlx, data->mlx.mlx_win,
+		data->img.img, 0, 0);
 }
