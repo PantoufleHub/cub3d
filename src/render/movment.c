1fa21e9905f33be2db9c871b0a59e1bf7fdead2d
Timothee Pierre-Andre Frily <tfrily@c3r4s3.42lausanne.ch>
2024-08-09 10:38:44 +0200

    Delete the is_move function

--- src/render/movment.c
+++ src/render/movment.c
@@ -2,14 +2,6 @@
 #include "../../inc/cub3D.h"
 #include <math.h>
 
-int is_move(int x, int y , t_data *data)
-{
-  // NOTE: RETURN TRUE if a the next position is a '0'. False if not
-  if (data->map[x][y] == '0')
-    return (TRUE);
-  return (FALSE);
-}
-
 int ft_keypress(int key, t_data *data)
 {
   if (key == K_W)
