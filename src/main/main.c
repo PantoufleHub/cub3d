9a7f3ba0fffb58047d15742b9c3d48c5eba8a1ac
Tim <timothee.frily@42lausanne.com>
2024-08-30 12:35:14 +0200

    modified the code in parsing.c to add {} to if statements

--- src/main/main.c
+++ src/main/main.c
@@ -4,11 +4,11 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (init_data(&data) < 0)
-		return (printf(RED"Error\nCouldn't initialize data\n"WHT), EXIT_FAILURE);
 	if (argc != 2)
 		return (printf(RED"Error\nPlease input single .cub file\n"WHT),
 			EXIT_FAILURE);
+	if (init_data(&data) < 0)
+		return (printf(RED"Error\nCouldn't initialize data\n"WHT), EXIT_FAILURE);
 	if (parse(argv[1], &data) < 0)
 		printf(RED"\n- Parsing failed -\n\n"WHT);
 	else
