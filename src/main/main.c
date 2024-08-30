50c8d593070f7fec6ac3f6aa313877aa9ae44428
SaitoZK <aperron@c2r1s6.42lausanne.ch>
2024-08-30 14:00:44 +0200

    added test files & corrected main

--- src/main/main.c
+++ src/main/main.c
@@ -8,14 +8,18 @@ int	main(int argc, char **argv)
 		return (printf(RED"Error\nPlease input single .cub file\n"WHT),
 			EXIT_FAILURE);
 	if (init_data(&data) < 0)
-		return (printf(RED"Error\nCouldn't initialize data\n"WHT), EXIT_FAILURE);
+		return (printf(RED"Error\nCouldn't initialize data\n"WHT),
+			EXIT_FAILURE);
 	if (parse(argv[1], &data) < 0)
-		printf(RED"\n- Parsing failed -\n\n"WHT);
+		return (printf(RED"\n- Parsing failed -\n\n"WHT), EXIT_FAILURE);
 	else
 		printf(GRN"\n- Parsing Succesful -\n\n"WHT);
+	printf("1\n");
 	ft_hooks(&data);
+	printf("2\n");
 	render(&data);
+	printf("3\n");
 	mlx_loop(data.mlx);
+	printf("4\n");
 	return (EXIT_SUCCESS);
 }
-
