7af0f041f7306427fc17ee306748c439cc9d3bef
SaitoZK <aperron@c2r1s6.42lausanne.ch>
2024-09-04 17:33:05 +0200

    Texture render v0.0.0.0.0.0.0.0.0.0.1

--- src/render/render.c
+++ src/render/render.c
@@ -19,17 +19,25 @@ void	render_column(t_line wall, int side, t_data *data)
 	// pixel_put_line(data->img, data->x, wall, color);
 	test = wall.drawStart;
 	int c;
+	// printf("Walldist : [%f]\n", data->calc_info.perpWallDist);
 	int wall_w = WIDTH / data->calc_info.perpWallDist;
 	int wall_h = HEIGHT / data->calc_info.perpWallDist;
 	int	step_x = wall_w / data->textures[0].width;
 	int	step_y = wall_h / data->textures[0].height;
+	if(step_y == 0)
+		step_y = 1;
 	// int c = create_trgb(0, 255, 0, 0);
 	// printf("Wall pos  [ %f , %f ]\n", data->calc_info.wall_pos.x, data->calc_info.wall_pos.y);
 	// printf("Wall size [ %d , %d ]\n", wall_w, wall_h);
 	while (test < wall.drawEnd)
 	{
 		// printf("CLACULDUCUL: %d, %d, %d, %d\n", test, wall.drawStart, wall.drawEnd, wall.drawEnd-wall.drawStart/4);
-		c = ((int *)(data->textures[0].img_data->addr))[((test - wall.drawStart)/ step_y) * data->textures[0].width + (int)((data->calc_info.wall_pos.x)/ step_x)];
+		int n = ((test - wall.drawStart)/ step_y) * data->textures[0].width
+			+ (int)((fmod(data->calc_info.wall_pos.x, 1.0) * (wall_w))/step_x);
+		// int n = fmod(data->calc_info.wall_pos.x, 1.0) * (double)data->textures[0].width;
+		if (n < 0 || n > data->textures[0].width * data->textures[0].height)
+			printf("tisouci\n"), n = 0;
+		c = ((int *)(data->textures[0].img_data->addr))[n];
 		// printf("Color: %d\n", c);
 		// mlx_pixel_put(data->mlx, data->win, data->x, test, c);
 		my_mlx_pixel_put(&data->img, data->x, test, c);
