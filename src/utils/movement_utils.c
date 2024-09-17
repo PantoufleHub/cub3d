d78a890d5be46a5a99f88f99ef31fe9f8e2d7c7f
Tim <timothee.frily@42lausanne.com>
2024-09-17 11:59:06 +0200

    Add the inbound check for x in forward and backward functions

--- src/utils/movement_utils.c
+++ src/utils/movement_utils.c
@@ -1,13 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfrily <tfrily@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 11:11:44 by tfrily            #+#    #+#             */
+/*   Updated: 2024/09/17 11:19:50 by tfrily           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/cub3D.h"
 
 int	w_is_in_bound(t_data *data, double move_speed)
 {
 	int	forward_y;
 	int	forward_x;
+	int	column_nbr;
 
 	forward_y = (int)(data->vec.pos.y + data->vec.dir.y * move_speed);
 	forward_x = (int)(data->vec.pos.x + data->vec.dir.x * move_speed);
 	if (data->nb_rows < forward_y)
+		return (FALSE);
+	column_nbr = ft_strlen(data->map[forward_y]);
+	if (column_nbr < forward_x)
 		return (FALSE);
 	else
 		return (TRUE);
@@ -17,10 +33,14 @@ int	s_is_in_bound(t_data *data, double move_speed)
 {
 	int	backward_y;
 	int	backward_x;
+	int	column_nbr;
 
 	backward_y = (int)(data->vec.pos.y - data->vec.dir.y * move_speed);
 	backward_x = (int)(data->vec.pos.x - data->vec.dir.x * move_speed);
 	if (data->nb_rows < backward_y)
+		return (FALSE);
+	column_nbr = ft_strlen(data->map[backward_y]);
+	if (column_nbr < backward_x)
 		return (FALSE);
 	else
 		return (TRUE);
