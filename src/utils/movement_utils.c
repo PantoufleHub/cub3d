9d2536e196f7c2afa4cefa3d522b4da8bea1adb6
SaitoZK <aperron@c1r11s7.42lausanne.ch>
2024-09-18 11:42:37 +0200

    Fixed out of bounds segfault

--- src/utils/movement_utils.c
+++ src/utils/movement_utils.c
@@ -6,7 +6,7 @@
 /*   By: aperron <aperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:16:08 by aperron           #+#    #+#             */
-/*   Updated: 2024/09/18 11:16:11 by aperron          ###   ########.fr       */
+/*   Updated: 2024/09/18 11:41:40 by aperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	w_is_in_bound(t_data *data, double move_speed)
 
 	forward_y = (int)(data->vec.pos.y + data->vec.dir.y * move_speed);
 	forward_x = (int)(data->vec.pos.x + data->vec.dir.x * move_speed);
-	if (data->nb_rows < forward_y)
+	if (data->nb_rows < forward_y || forward_y < 0)
 		return (FALSE);
 	column_nbr = ft_strlen(data->map[forward_y]);
-	if (column_nbr < forward_x)
+	if (column_nbr < forward_x || forward_x < 0)
 		return (FALSE);
 	else
 		return (TRUE);
@@ -37,10 +37,10 @@ int	s_is_in_bound(t_data *data, double move_speed)
 
 	backward_y = (int)(data->vec.pos.y - data->vec.dir.y * move_speed);
 	backward_x = (int)(data->vec.pos.x - data->vec.dir.x * move_speed);
-	if (data->nb_rows < backward_y)
+	if (data->nb_rows < backward_y || backward_y < 0)
 		return (FALSE);
 	column_nbr = ft_strlen(data->map[backward_y]);
-	if (column_nbr < backward_x)
+	if (column_nbr < backward_x || backward_x < 0)
 		return (FALSE);
 	else
 		return (TRUE);
